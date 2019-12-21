509be8048136fb0fd6ec14263ff8cf4011eaa497
Egor Rumyantsev <RumyantsevGT@ya.ru>
2019-12-21 18:23:00 +0300

    Add files via upload

---
+++ upfile/dist/script.js
@@ -0,0 +1,15 @@
+const ipfs = window.IpfsHttpClient('ipfs.infura.io', '5001', { protocol: 'https' });
+
+$("#upload").on("change", function() {
+  var reader = new FileReader();
+  reader.onload = function (e) {
+
+    const magic_array_buffer_converted_to_buffer = buffer.Buffer(reader.result);
+    ipfs.add(magic_array_buffer_converted_to_buffer, (err, result) => {
+      console.log(err, result);
+      let ipfsLink = "<a href='https://gateway.ipfs.io/ipfs/" + result[0].hash + "'>gateway.ipfs.io/ipfs/" + result[0].hash + "</a>";
+      document.getElementById("link").innerHTML = ipfsLink;
+    })
+  }
+  reader.readAsArrayBuffer(this.files[0]);
+})
